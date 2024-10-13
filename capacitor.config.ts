fd66be57e4ba36455eff7ea6ab7ff8e42229fc76
Milind Diwadkar <165246230+milinddiwadkar@users.noreply.github.com>
2024-10-13 21:23:50 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'H5P Content',
+  webDir: 'www'
+};
+
+export default config;
